11c852e612653039c255bd5e13962107f27b93b9
Bima Saridjo <palessie27@gmail.com>
2020-07-25 12:47:16 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
+const path = require('path');
+
 module.exports = {
     module: {
+      entry: path.join(__dirname, './client/index.js'),
+      mode: 'development',
+      devtool: 'source-map',
+      output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
+      },
       rules: [
         {
           test:/\.js$/,
