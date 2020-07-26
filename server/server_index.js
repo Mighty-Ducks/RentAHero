427ae657e97d358334423e658c8910963e3707cd
Bima Saridjo <palessie27@gmail.com>
2020-07-26 10:23:03 -0400

    updating branch with new master changes

--- server/server_index.js
+++ server/server_index.js
@@ -1,1 +1,17 @@
 // Initialize express and calles app.listen
+const express = require('express');
+const apiRouter = require('./routes/routes_index');
+
+const app = express();
+
+app.use('/api', apiRouter);
+
+const startServer = (PORT) => {
+    return new Promise ((res) => {
+        app.listen(PORT, () => {
+            console.log(`Server is now running at PORT: ${PORT}`);
+        })
+    })
+}
+
+module.exports = app;
