2ce93876e885cfa5651cb36d4f839b555118352d
DmytroLypovetskiy <lipovetskiy@gmail.com>
2020-07-28 16:28:02 -0400

    Fixed eslint

--- client/components/home/Home.js
+++ client/components/home/Home.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <div>
-      <h2>I will miss you</h2>
-      <img
-        alt="I will miss you"
-        src="./assets/stormtrooper.png"
-        height="400"
-      />
-    </div>;
+    return (
+      <div>
+        <h2>I will miss you</h2>
+        <img
+          alt="I will miss you"
+          src="./assets/stormtrooper.png"
+          height="400"
+        />
+      </div>
+    );
   }
 }
