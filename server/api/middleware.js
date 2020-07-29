8a34dedfe8f60b513cf8eb40a2730f5706b4b010
Bima Saridjo <palessie27@gmail.com>
2020-07-29 14:09:38 -0400

    updated middleware

--- server/api/middleware.js
+++ server/api/middleware.js
@@ -5,6 +5,8 @@ const app = require('./server');
 const DIST_PATH = path.join(__dirname, '../../dist');
 const PUBLIC_PATH = path.join(__dirname, '../../public');
 
+app.use(express.json());
+
 const expressMiddleware = () => {
   app.use('/dist', express.static(DIST_PATH));
   app.use(express.static(PUBLIC_PATH));
