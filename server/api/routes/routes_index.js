3570b1ed50a95b4b2a5896d9e19edfe505ca27c0
Bima Saridjo <palessie27@gmail.com>
2020-07-29 12:46:29 -0400

    push new routes, and updated middleware

--- server/api/routes/routes_index.js
+++ server/api/routes/routes_index.js
@@ -1,9 +1,13 @@
 // app.use for all the diffrent routes
 const superheroesRouter = require('./superheroes_routes');
+const actsRouter = require('./acts_routes');
+const categoriesRouter = require('./categories_routes');
 const app = require('../server');
 
 const applyRoutes = () => {
   app.use('/api/superheroes', superheroesRouter);
+  app.use('/api/acts', actsRouter);
+  app.use('/api/categories', categoriesRouter);
 };
 
 module.exports = applyRoutes;
