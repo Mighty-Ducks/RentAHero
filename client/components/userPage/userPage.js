a19d9d85e1117f3a39187fa1400dfde1a5d1ca06
Bima Saridjo <palessie27@gmail.com>
2020-08-11 00:52:27 -0400

    added modal for edit user

--- client/components/userPage/userPage.js
+++ client/components/userPage/userPage.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import UserOrders from '../userOrders/UserOrders';
+import EditUserForm from '../editUserForm/EditUserForm';
+import Popup from '../popup/Popup';
+import EditUserButton from '../buttons/EditUserButton';
 import './userPage.scss';
 
 class UserPage extends Component {
   render() {
-    const { firstName, lastName, user } = this.props;
+    const { firstName, lastName, user, address } = this.props;
     return (
       <div className="hero-view container-xl">
         <h1>Profile Info</h1>
         <ul>
           <li>
-            First Name:&nbsp;
+            Name:&nbsp;
             {firstName}
-          </li>
-          <li>
-            Last Name:&nbsp;
+            &nbsp;
             {lastName}
           </li>
           <li>
             Email:&nbsp;
             {user}
           </li>
+          <li>
+            Address:&nbsp;
+            {address}
+          </li>
+          <Popup
+            title="Edit Hero"
+            BodyModal={EditUserForm}
+            ButtonModal={EditUserButton}
+            data={{ firstName, lastName, user, address }}
+          />
         </ul>
         <UserOrders {...this.props} />
       </div>
